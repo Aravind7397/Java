081fbd117d5b2aa2c67eb3ee936cd87c2ede4cc1
Aravind <aravindgovindaraj777@gmail.com>
2022-09-15 23:31:50 +0530

    Parsing

---
+++ Coding/src/Parsing.java
@@ -0,0 +1,8 @@
+public class Parsing {
+    public static void toParse(String value){
+        int integer = Integer.parseInt(value);
+        System.out.println(integer);
+        double d = Double.parseDouble(value);
+        System.out.println(d);
+    }
+}
